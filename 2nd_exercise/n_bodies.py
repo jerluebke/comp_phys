adf85ef27ddc59a961224d0756cb4d0339256902
Jeremiah Lübke <jer.luebke@web.de>
2018-11-04 13:42:51 +0100

    animated n bodies, updated gitignore

--- 2nd_exercise/n_bodies.py
+++ 2nd_exercise/n_bodies.py
@@ -3,6 +3,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
+plt.rcParams['animation.ffmpeg_path'] ='D:\\source\\Libs\\ffmpeg-20180521-c24d247-win64-static\\bin\\ffmpeg.exe'
+
 
 def solve_all_two(q0, q1, m, dt, steps, F, *fargs, **fkwds):
     res = np.zeros((2, 2, steps))   # particles, dimensions, steps
@@ -77,7 +79,7 @@ def get_dist(q):
     n = q.shape[1]  # dimensions
     rv = np.zeros((p, p, n))    # distance vectors
     for i in range(p):
-        for j in range(p):
+        for j in range(i):
             rv[i,j,:] = q[j,:] - q[i,:]     # distances between each particle
     rm = np.sum(rv**2, axis=2)  # distance magnitutes
     #  return np.ma.masked_where(dv==0,dv), np.ma.masked_where(dm==0,dm)
@@ -86,8 +88,12 @@ def get_dist(q):
 def F_n(rv, rm, m, i):
     p = m.size
     return np.sum(
-        [m[j]*rv[i,j,:]/rm[i,j]**(3./2.)
-        for j in range(p) if j != i],
+            [m[j]*rv[i,j,:]/rm[i,j]**(3./2.)
+            for j in range(i)],
+        axis=0) \
+        - np.sum(
+            [m[j]*rv[j,i,:]/rm[j,i]**(3./2.)
+            for j in range(i+1, p)],
         axis=0)
 
 
@@ -105,12 +111,13 @@ q1 = q0 + np.array([
 ])
 m = np.array([1., 1., 1., 1.])
 #  m = np.array([10., 2., 1.])
-res = solve_all_n(q0, q1, m, .05, 500, F_n)
-plt.figure()
-[plt.plot(res[i,0,:], res[i,1,:]) for i in range(m.size)]
+STEPS = 1000
+res = solve_all_n(q0, q1, m, .05, STEPS, F_n)
+#  plt.figure()
+#  [plt.plot(res[i,0,:], res[i,1,:]) for i in range(m.size)]
 
 fig = plt.figure()
-ax = fig.add_subplot(111, xlim=(-10, 10), ylim=(-10, 10))
+ax = fig.add_subplot(111, xlim=(-5, 5), ylim=(-5, 5))
 lines = [ax.plot([], [], 'b--', zorder=1)[0] for _ in range(m.size)]
 points = [ax.plot([], [], 'ro', zorder=2)[0] for _ in range(m.size)]
 
@@ -126,5 +133,9 @@ def step(i):
         points[j].set_data(res[j,0,i], res[j,1,i])
     return [*lines, *points]
 
-#  anim = animation.FuncAnimation(fig, step, frames=500, interval=100, blit=True,
-#                                 repeat=False, init_func=init)
+
+anim = animation.FuncAnimation(fig, step, frames=STEPS, interval=20, blit=True,
+                               repeat=False, init_func=init)
+
+FFWriter = animation.FFMpegWriter(fps=60)
+anim.save('4_bodies.mp4', writer=FFWriter, dpi=300)
