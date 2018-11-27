96f98619ec7e37e9f3c2d1d16b8887fc6f1582ea
Jeremiah Lübke <jer.luebke@web.de>
2018-11-27 22:45:19 +0100

    figured out inverse dft

--- 6th_exercise/navier_stokes/src/test.c
+++ 6th_exercise/navier_stokes/src/test.c
@@ -1,7 +1,9 @@
 #define _USE_MATH_DEFINES
-#include <stdlib.h>
-#include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
 #include "../include/navier_stokes.h"
 
 
@@ -54,30 +56,26 @@ double inital_func(double x, double y)
 }
 
 static inline
-void rfftshift(double *arr, size_t n0, size_t n1, char sign)
+void rfftshift(double *arr, size_t n0, size_t n1)
 {
     size_t i, j, k;
-    double dsign;
-    dsign = sign > 0 ? 1. : -1.;
 
-    /* for (i = 0; i < n0; ++i) */
-    /*     for (j = 0; j < n1; ++j) */
-    /*         arr[j+i*n1] *= dsign * ((i + j) % 2 == 0 ? -1 : 1); */
-    /* for (i = 0; i < n0*n1; ++i)
-     *     arr[i] *= i % 2 == 0 ? -1 : 1; */
     for (i = 0, k = 0; i < n1; ++i)
         for (j = 0; j < n0; ++j, ++k) {
+            /* center zero-mode in the middle and flip along x-axis */
             arr[j+i*n0] *= (i+j) % 2 == 0 ? -1 : 1;
+            /* revert x-axis flip */
             arr[k] *= k % 2 == 0 ? -1 : 1;
         }
 }
+
 
 int main()
 {
     size_t i, j, Nx, Ny, Nkx, Nky, Ntot, Nktot;
     double xmin, xmax, ymin, ymax;
-    double *x, *y, *z;
-    fftw_complex *z_hat;
+    double *x, *y, *z, *z_bu;
+    fftw_complex *z_hat, *z_hat_bu;
 
     Nx = 16, Ny = 16;
     Nkx = Nx / 2 + 1, Nky = Ny;
@@ -88,9 +86,12 @@ int main()
     x = malloc(sizeof(*x) * Nx);
     y = malloc(sizeof(*y) * Ny);
     z = fftw_alloc_real(Ntot);
+    z_bu = fftw_alloc_real(Ntot);
     z_hat = fftw_alloc_complex(Nktot);
+    z_hat_bu = fftw_alloc_complex(Nktot);
 
     fftw_plan rfft2 = fftw_plan_dft_r2c_2d(Nx, Ny, z, z_hat, FFTW_MEASURE);
+    fftw_plan irfft2 = fftw_plan_dft_c2r_2d(Nx, Ny, z_hat, z, FFTW_MEASURE);
 
     rlinspace(xmin, xmax, Nx, x);
     rlinspace(ymin, ymax, Ny, y);
@@ -98,15 +99,35 @@ int main()
         for (j = 0; j < Nx; ++j)
             z[j+i*Nx] = -inital_func(x[j], y[i]);
 
+    /* [> to measure execution time in ns: <]
+     * struct timespec tp0, tp_a, tp_b;
+     * tp0.tv_sec = 0, tp0.tv_nsec = 0;
+     * clock_settime(0, &tp0);
+     * clock_gettime(0, &tp_a);
+     * [> do something... <]
+     * clock_gettime(0, &tp_b);
+     * printf("time:\t%ld ns\n\n", tp_b.tv_nsec-tp_a.tv_nsec); */
+
     /* print_real_array(z, Nx, Ny); */
 
-    rfftshift(z, Nx, Ny, 1);
+    /* Fourier Transform */
+    rfftshift(z, Nx, Ny);
     fftw_execute(rfft2);
-    /* rfftshift(z, Nx, Ny, 1); */
+    rfftshift(z, Nx, Ny);
+
 
     /* print_real_array(z, Nx, Ny); */
+    /* print_complex_array(z_hat, Nkx, Nky); */
 
-    print_complex_array(z_hat, Nkx, Nky);
+
+    /* inverse Fourier Transform */
+    memcpy((void *)z_hat_bu, (void *)z_hat, Nktot * sizeof(fftw_complex));
+    fftw_execute(irfft2);
+    rfftshift(z, Nx, Ny);
+
+    /* print_complex_array(z_hat_bu, Nkx, Nky); */
+    /* print_real_array(z, Nx, Ny); */
+
 
     /* Params p = {Nx, Ny, .05, .0}; */
     /* Workspace *pde = init(p, z); */
@@ -124,7 +145,9 @@ int main()
     free(x);
     free(y);
     fftw_free(z);
+    fftw_free(z_bu);
     fftw_free(z_hat);
+    fftw_free(z_hat_bu);
     fftw_destroy_plan(rfft2);
     fftw_cleanup();
 
